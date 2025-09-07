fc52b6c6a843f59ef37316486f10a84b9bd09c45
Felipe Greg <mrfelipesensei@gmail.com>
2025-09-06 21:41:24 -0300

    dicionário alimentos pt1

---
+++ macros/index.py
@@ -0,0 +1,17 @@
+alimentos = {
+    "ovo cozido" : {
+        "proteina" : 6,
+        "carboidrato" : 0.27,
+        "gordura" : 4.3
+    },
+    "ovo frito" : {
+        "proteina" : 7.8,
+        "carboidrato" : 0.6,
+        "gordura" : 9.3
+    },
+    "arroz branco cozido" : {
+        "proteina" : 2.5,
+        "carboidrato" : 28,
+        "gordura" : 0.2
+    }
+}
