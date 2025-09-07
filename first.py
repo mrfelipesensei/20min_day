f86c02fb2313ebb6adb27d842219cb8354d5f325
Felipe Greg <mrfelipesensei@gmail.com>
2025-09-06 21:30:04 -0300

    lista básica

---
+++ first.py
@@ -0,0 +1,23 @@
+#Revisão listas
+list = [1, 2 , 3, 4, 5]
+
+#Acessando o primeiro item da lista
+print(list[0])
+
+#Acessar o último item da lista
+print(list[4])
+
+#Adicionando número no final
+list.append(6)
+
+print(list)
+
+#Remover o segundo elemento
+list.remove(list[1])
+
+print(list)
+
+#Ordenar a lista em ordem decrescente
+list.sort(reverse=True)
+
+print(list)
