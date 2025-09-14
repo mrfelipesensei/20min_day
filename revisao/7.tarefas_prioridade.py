ee169ef48b1131a2673c011a6aaecd63144b1876
Felipe Greg <mrfelipesensei@gmail.com>
2025-09-14 18:41:24 -0300

    tarefas_prioridade

---
+++ revisao/7.tarefas_prioridade.py
@@ -0,0 +1,30 @@
+#Criando o dicionário de tarefas vazio
+tarefas_dia = {}
+
+#Enquanto o programa não for encerrado
+while True:
+    print("1 - Cadastrar Tarefa")
+    print("2 - Alterar Prioridade")
+    print("3 - Remover Tarefa")
+    print("4 - Visualizar tarefas")
+
+    escolha = input("Digite sua opção: ")
+
+    if escolha == "1":
+        #Lógica da cadastro de tarefas
+    
+    elif escolha == "2":
+        #Lógica de alterar prioridade da tarefa
+
+    elif escolha == "3":
+        #Deletar tarefa cadastrada
+
+    elif escolha == "4":
+        #Mostrar tarefas cadastradas
+
+    elif escolha == "5":
+        print("Saindo do programa. Até mais!")
+        break
+
+    else:
+        print("Opção inválida, tente novamente.")
