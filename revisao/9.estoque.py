8cbf57f74f5d3ed7741317e65ef2b6ce2c561908
Felipe Greg <mrfelipesensei@gmail.com>
2025-09-22 15:40:30 -0300

    alterando lógica + adicionar_items()

--- revisao/9.estoque.py
+++ revisao/9.estoque.py
@@ -1,16 +1,30 @@
 #Carregando dicionário inicial vazio
-lista_compras = {}
+estoque = {}
+
+def adicionar_item():
+    nome = input("Digite o nome do item: ").strip()
+
+    valor = float(input(f"Digite o valor de {nome} R$: "))
+
+    quantidade = int(input(f"Digite a quantidade: "))
+
+    categoria = input(f"Digite a categoria de {nome}: ")
+
+    #Adicionar ao dicionário
+    estoque[nome] = valor, quantidade, categoria
+    print(f"{nome} adicionado com sucesso.")
+
 
 
 while True:
-    print("\n--- LISTA DE COMPRAS ---")
+    print("\n--- CONTROLE DE ESTOQUE ---")
     print("1 - Adicionar Item")
     print("2 - Listar Itens")
     print("3 - Filtar por Categoria")
     print("4 - Calcular Valor Total")
     print("5 - Alterar Dados")
     print("6 - Salvar Dados")
-    print("7 - ")
+    print("7 - Sair")
 
     escolha = input("Digite sua opção: ")
 
@@ -26,7 +40,7 @@ while True:
         alterar_dados()
     elif escolha == "6":
         salvar_dados()
-        print("Lista de Compras salvas no arquivo lista_compras.json")
+        print("Controle de Estoque salvo no arquivo lista_compras.json")
     elif escolha == "7":
         print("\nSaindo do programa. Até mais!")
         break
