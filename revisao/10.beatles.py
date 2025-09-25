5c44700a81cfe8faa8912e5fc2cd0a8df97b72c0
Felipe Greg <mrfelipesensei@gmail.com>
2025-09-25 19:25:57 -0300

    lógica de musicas I

--- revisao/10.beatles.py
+++ revisao/10.beatles.py
@@ -1,4 +1,74 @@
+from datetime import datetime
+
+
 discos = {}
+
+def adicionar_disco():
+
+    while True:
+        nome = input("Digite o Nome do Disco: ").strip()
+
+        if not nome:
+            print("ERRO - O nome não pode estar em branco.")
+            continue
+
+        if not all(c.isalpha() or c.isspace() for c in nome):
+            print("ERRO - O nome deve conter apenas letras e espaços.")
+            continue
+
+        break
+
+    while True:
+
+        entrada = input("Digite uma data (dd/mm/aaaa): ")
+
+        try:
+            data = datetime.strptime(entrada, "%d/%m/%Y")
+            print("Data válida: ", data.strftime("%d/%m/%Y"))
+            break
+        except ValueError:
+            print("Data inválida! Use o formato dd/mm/aaaa.")
+            continue
+    
+    while True:
+
+        gravadora = input("Digite o nome da Gravadora: ")
+
+        if not gravadora:
+            print("ERRO - A Gravadora não pode estar em branco.")
+            continue
+
+        if not all(c.isalpha() or c.isspace() for c in gravadora):
+            print("ERRO - A Gravadora deve conter apenas letras e espaços.")
+            continue
+
+        break
+
+    musicas = []
+
+    while True:
+        titulo = input("Digite o título da música: ").strip()
+
+        if not titulo:
+            print("ERRO - O título não pode estar em branco.")
+            continue
+
+        if not all(c.isalpha() or c.isspace() for c in titulo):
+            print("ERRO - O título deve conter apenas letras e espaços.")
+            continue
+
+        musicas.append({"titulo":titulo})
+
+        continuar = input("Deseja adicionar outra música? (s/n): ").strip().lower()
+        if continuar == "n":
+            break
+        else:
+            continue
+
+    
+    discos[nome] = entrada, gravadora, musicas
+    print(f"{nome} adicionado com sucesso.")
+    
 
 while True:
     print("\n--- DISCOGRAFIA BEATLES ---")
