1c789e5c561a0ae58f760369994cc100e2f664f2
Felipe Greg <mrfelipesensei@gmail.com>
2025-10-06 18:13:12 -0300

    pergunta_dev()

--- modulacao/funcoes.py
+++ modulacao/funcoes.py
@@ -15,10 +15,30 @@ def pergunta_titulo():
 
     return titulo
 
+def pergunta_dev():
+
+    while True:
+        desenvolvedora = input("Digite o nome da Desenvolvedora: ")
+
+        if not desenvolvedora:
+            print("ERRO - O nome da Desenvolvedora não pode estar em branco.")
+            continue
+
+        elif not all(c.isalpha() or c.isspace() for c in desenvolvedora):
+            print("ERRO - O nome da desenvolvedora deve conter apenas letras e espaços.")
+            continue
+        else:
+            break
+    
+    return desenvolvedora
 
 def adicionar_jogo():
 
     titulo = pergunta_titulo()
+
+    desenvolvedora = pergunta_dev()
+
+    
 
 
 
